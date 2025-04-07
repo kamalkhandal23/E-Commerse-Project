1cb68ceaab088ad8ae6efc52a001c3776572c983
Kamal Kishore Bochiwal <108949333+kamalkhandal23@users.noreply.github.com>
2025-04-07 13:19:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, FaEnvelope } from "@fortawesome/free-solid-svg-icons";
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
